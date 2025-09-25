2be56ce68df0b99a2e1785584721c7a297b69456
Rongkun <rongkun2003@gmail.com>
2025-09-25 14:56:11 +0800

    Dynamic character movement

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -48,40 +48,70 @@ public class Player extends Entity{
             y -= speed;
             direction = "up";
         }
-        if (keyH.downPressed) {
+        else if (keyH.downPressed) {
             y += speed;
             direction = "down";
         }
-        if (keyH.rightPressed) {
+        else if (keyH.rightPressed) {
             x += speed;
             direction = "right";
         }
-        if (keyH.leftPressed) {
+        else if (keyH.leftPressed) {
             x -= speed;
             direction = "left";
+        }
+        spriteCounter++;
+        if (spriteCounter > 10) { //Player image changes every ~10 frames
+            if (spriteNum == 1) {
+                spriteNum = 2;
+            }
+            else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
         }
     }
     public void draw(Graphics2D g2) {
         BufferedImage image = null;
-        switch (direction) {
-            case "up" -> image = up1;
-            case "down" -> image = down1;
-            case "right" -> image = right1;
-            case "left" -> image = left1;
-            default -> image =  null;
-//        switch(direction) {
-//            case "up":
-//                image = up1;
-//            break;
-//            case "down":
-//                image = down1;
-//            break;
-//            case "right":
-//                image = right1;
-//            break;
-//            case "left":
-//                image = left1;
-//            break;
+//        switch (direction) {
+//            case "up" -> image = up1;
+//            case "down" -> image = down1;
+//            case "right" -> image = right1;
+//            case "left" -> image = left1;
+//            default -> image =  null;
+        switch(direction) {
+            case "up":
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
+            break;
+            case "down":
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
+            break;
+            case "right":
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
+            break;
+            case "left":
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
+            break;
         }
 
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
